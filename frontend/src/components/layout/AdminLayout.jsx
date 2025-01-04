b8df2b51b54bc227a337cf3009233d27dda5e5fe
ajit <ajit@aidev.co.in>
2025-01-04 12:33:19 +0530

    admin side bar

--- frontend/src/components/layout/AdminLayout.jsx
+++ frontend/src/components/layout/AdminLayout.jsx
@@ -1,10 +1,82 @@
-import { Box, Drawer, Grid2, IconButton, Menu } from "@mui/material";
+import {
+  Box,
+  Drawer,
+  Grid2,
+  IconButton,
+  Menu,
+  Stack,
+  styled,
+  Typography,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 import { useState } from "react";
+import { useLocation } from "react-router";
+// import { Link } from "../styles/styledComponents";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PeopleIcon from "@mui/icons-material/People";
+import GroupsIcon from "@mui/icons-material/Groups";
+import { Link as LinkComponent } from "react-router";
 
-const Sidebar = () => {
-  return <Box>side bar asdfadsfdasfasdfdas</Box>;
+const Link = styled(LinkComponent)({
+  textDecoration: "none",
+  borderRadius: "2rem",
+  color: "black",
+  padding: "1rem 2rem",
+  "&:hover": {
+    backgroundColor: "rgba(0,0,0,0.1)",
+  },
+});
+
+export const adminTab = [
+  {
+    name: "Dashboard",
+    link: "/admin/dashboard",
+    icon: <DashboardIcon />,
+  },
+  {
+    name: "Users",
+    link: "/admin/users-management",
+    icon: <PeopleIcon />,
+  },
+  {
+    name: "Chats",
+    link: "/admin/chats-managemnet",
+    icon: <GroupsIcon />,
+  },
+  {
+    name: "messages",
+    link: "/admin/messages-managemnet",
+    icon: <GroupsIcon />,
+  },
+];
+
+const Sidebar = ({ w = "50vw" }) => {
+  const location = useLocation();
+  console.log(location);
+  return (
+    <>
+      <Stack width={w} direction={"column"} padding={"3rem"}>
+        <Typography variant="h5">Admin Dashboard</Typography>
+      </Stack>
+      <Stack spacing={"1rem"}>
+        {adminTab.map((tab) => (
+          <Link key={tab.name} to={tab.link}>
+            <Stack
+              direction={"row"}
+              spacing={"1rem"}
+              alignItems={"center"}
+              padding={"1rem"}
+              bgcolor={location.pathname === tab.link ? "lightgray" : ""}
+            >
+              {tab.icon}
+              <Typography>{tab.name}</Typography>
+            </Stack>
+          </Link>
+        ))}
+      </Stack>
+    </>
+  );
 };
 
 const AdminLayout = ({ children }) => {
@@ -41,7 +113,17 @@ const AdminLayout = ({ children }) => {
         {children}
       </Grid2>
       {isMobile && (
-        <Drawer anchor="right" open={isMobile} onClose={handleMobile}>
+        <Drawer
+          sx={{
+            display: {
+              xs: "block",
+              md: "none",
+            },
+          }}
+          anchor="right"
+          open={isMobile}
+          onClose={handleMobile}
+        >
           {isMobile && (
             <IconButton
               sx={{ position: "fixed", top: "1rem", right: "1rem" }}
@@ -50,7 +132,7 @@ const AdminLayout = ({ children }) => {
               <CloseIcon />
             </IconButton>
           )}
-          <Sidebar />
+          <Sidebar width="50vw" />
         </Drawer>
       )}
     </Grid2>
